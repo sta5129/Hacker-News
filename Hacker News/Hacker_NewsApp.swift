e6b9ea7b72531276f3eb305456af7fe5f017cc48
Samarah Anderson <samarahtanderson@gmail.com>
2023-06-12 15:40:38 -0400

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Samarah Anderson on 6/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
